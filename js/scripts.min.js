34d27b146f3d7e1880c013d4330c3aca8039faee
Tayirov Bakhtiyar <bakhtiyarkhozha@gmail.com>
2019-09-02 12:10:39 +0500

    adaptive

--- js/scripts.min.js
+++ js/scripts.min.js
@@ -1,9 +1,21 @@
 $(function() {
 
 	$('.slider').slick({
-		 dots: true,
+		dots: true,
 	  infinite: true,
 	  speed: 300,
 	});
 
+$(".right-menu__btn").click(function(evt){
+	evt.preventDefault();
+	$('.right-menu .search-wrap').addClass('active');
+	$('.search-close').addClass('active');
+	$(".header-top__form").addClass('active');
+});
+
+$('.search-close').on('click', function(){
+	$('.right-menu .search-wrap').removeClass('active');
+	$('.search-close').removeClass('active');
+	$('.header-top__form').removeClass('active');
+});
 });
