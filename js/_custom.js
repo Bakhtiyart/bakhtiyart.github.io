529e9f83f4bddfa34612298fa36e6caf204e1c16
Tayirov Bakhtiyar <bakhtiyarkhozha@gmail.com>
2019-09-14 13:48:44 +0500

    brands и выключение сетки bootstrap

--- js/_custom.js
+++ js/_custom.js
@@ -14,9 +14,37 @@ $(function() {
         }
     );
     wow.init();
+
+        // menu fixed on scroll
+    $(window).scroll(function() {
+        var scrolled = $(window).scrollTop();
+        if ( scrolled >= 150  ) {
+            $(".header-top-wrap").addClass("fixed");
+
+        } else {
+            $(".header-top-wrap").removeClass("fixed");
+        }
+    });
+
+     // anchors
+    $('a[href^="#"]').on("click", function (e) {
+        var anchor = $(this);
+        if(!anchor.hasClass('btn')){
+            $('a[href^="#"]').parent().removeClass('menu__item--active');
+            anchor.parent().addClass('menu__item--active');
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 80
+            }, 777);
+            e.preventDefault();
+            return false;
+        }
+
+    });
+
     $(".hamburger").click(function(){
         $(this).toggleClass('is-active');
-        $('.menu').toggleClass('menu-active');
+        $('.navbar').toggleClass('menu-active');
+        $('.header-top-wrap').toggleClass("background-blue");
     });
 
     $('.case-slider').slick({
@@ -27,6 +55,12 @@ $(function() {
         prevArrow: $('.prev-button'),
         nextArrow: $('.next-button')
     });
+
+    // $('.brands li').mouseenter(function(){
+    //     var atribut = $(this).attr('style');
+    //     console.log(atribut);
+    //     $(this).css("background", "red");
+    // } )//.mouseleave( handlerOut );
 
 
 });
