724f04e8a4f1e5b9d60660d67c6b33795ecb571e
Tayirov Bakhtiyar <bakhtiyarkhozha@gmail.com>
2019-10-20 20:27:46 +0500

    baiseken site

--- js/_custom.js
+++ js/_custom.js
@@ -1,71 +1,31 @@
 $(function() {
-    var wow = new WOW(
-        {
-            boxClass:     'wow',      // animated element css class (default is wow)
-            animateClass: 'animated', // animation css class (default is animated)
-            offset:       0,          // distance to the element when triggering the animation (default is 0)
-            mobile:       true,       // trigger animations on mobile devices (default is true)
-            live:         true,       // act on asynchronously loaded content (default is true)
-            callback:     function(box) {
-                // the callback is fired every time an animation is started
-                // the argument that is passed in is the DOM node being animated
-            },
-            scrollContainer: null // optional scroll container selector, otherwise use window
-        }
-    );
-    wow.init();
+	new WOW().init();
+	
+	$('.menu-icon').click(function(){
+		$(this).toggleClass('active');
+		if($('.menu').hasClass('show') || $('.menu').hasClass('active')){
+			$('.menu').removeClass('active');			
+			$('.overlay').removeClass('active');
+		 setTimeout(function() {
+			$('.menu').removeClass('show');
+		 }, 500);
+		}
+		else{
+			$('.menu').addClass('show');
+			$('.overlay').addClass('active');
+			setTimeout(function(){
+				$('.menu').addClass('active');
+			}, 100);
+		}s
+	});
 
-        // menu fixed on scroll
-    $(window).scroll(function() {
-        var scrolled = $(window).scrollTop();
-        if ( scrolled >= 150  ) {
-            $(".header-top-wrap").addClass("fixed");
-
-        } else {
-            $(".header-top-wrap").removeClass("fixed");
-        }
-    });
-
-     // anchors
-    $('a[href^="#"]').on("click", function (e) {
-        var anchor = $(this);
-        if(!anchor.hasClass('btn')){
-            $('a[href^="#"]').parent().removeClass('menu__item--active');
-            anchor.parent().addClass('menu__item--active');
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top - 80
-            }, 777);
-            e.preventDefault();
-            return false;
-        }
-
-    });
-
-    $("#header-hamburger").click(function(){
-        $(this).toggleClass('is-active');
-        $('.navbar').toggleClass('menu-active');
-        $('.header-top-wrap').toggleClass("background-blue");
-    });
-
-    $("#footer-hamburger").click(function(){
-        $(this).toggleClass('is-active');
-        $('.footer-phone-wrap').toggleClass('is--active');
-    });
-
-    $('.case-slider').slick({
-        dots: true,
-        infinite: false,
-        speed: 300,
-        slidesToShow: 1,
-        prevArrow: $('.prev-button'),
-        nextArrow: $('.next-button')
-    });
-
-    // $('.brands li').mouseenter(function(){
-    //     var atribut = $(this).attr('style');
-    //     console.log(atribut);
-    //     $(this).css("background", "red");
-    // } )//.mouseleave( handlerOut );
-
+	$('.overlay').click(function(){
+		$(this).removeClass('active');
+		$('.menu-icon').removeClass('active');
+		$('.menu').removeClass('active');
+		setTimeout(function() {
+			$('.menu').removeClass('show');
+		 }, 500);
+	});
 
 });
