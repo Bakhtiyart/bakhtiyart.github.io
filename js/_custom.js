0bc83100020ad9a54c64a4bdfa39f61ff6b4206c
Tayirov Bakhtiyar <bakhtiyarkhozha@gmail.com>
2019-10-20 20:44:35 +0500

    Обновление верстки baiseken

--- js/_custom.js
+++ js/_custom.js
@@ -28,4 +28,31 @@ $(function() {
 		 }, 500);
 	});
 
+
+    // menu fixed on scroll
+    $(window).scroll(function() {
+        var scrolled = $(window).scrollTop();
+        if ( scrolled >= 150  ) {
+            $(".header-top").addClass("fixed");
+
+        } else {
+            $(".header-top").removeClass("fixed");
+        }
+    });
+
+     // anchors
+    $('a[href^="#"]').on("click", function (e) {
+        var anchor = $(this);
+        if(!anchor.hasClass('btn')){
+            $('a[href^="#"]').parent().removeClass('menu__item--active');
+            anchor.parent().addClass('menu__item--active');
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 80
+            }, 777);
+            e.preventDefault();
+            return false;
+        }
+
+    });
+
 });
